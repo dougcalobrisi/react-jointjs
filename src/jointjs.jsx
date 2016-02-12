bc52b941093486124dfea61c221fc8c1f8893b1f
Doug Calobrisi <doug@calobrisi.com>
2016-02-12 16:27:30 -0500

    eslint, npm, and webpack build config

---
+++ src/jointjs.jsx
@@ -0,0 +1,26 @@
+import jointjs from 'jointjs';
+import React, { Component } from 'react';
+
+export default class ReactJointJS extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const oldProps = this.props;
+   }
+
+  componentWillUnmount() {
+  }
+
+  render() {
+    return (
+      <div>
+        JointJS
+      </div>
+    );
+  }
+}
